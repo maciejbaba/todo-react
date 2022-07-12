77b279b972f633dbc8b44c0d98deff9cca17c904
mackotron <maciek2baba2@gmail.com>
2022-07-12 10:16:33 +0200

    Fix not saving todos in the local storage

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
 import React, { useState, useRef, useEffect } from 'react';
 import TodoList from './TodoList';
-import uuidv4 from 'uuid/v4'
+import { v4 as uuidv4 } from 'uuid';
 
-const LOCAL_STORAGE_KEY = 'todoApp.todos'
+const LOCAL_STORAGE_KEY = 'todoApp.todos';
+
+const storedTodos = localStorage.getItem(LOCAL_STORAGE_KEY)
+  ? JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY)) : [];
 
 function App() {
-  const [todos, setTodos] = useState([]);
-  const todoNameRef = useRef()
-
-  useEffect(() => {
-    const storedTodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY))
-    if (storedTodos) { setTodos(storedTodos) }
-  }, []);
+  const [todos, setTodos] = useState(storedTodos);
+  const todoNameRef = useRef();
 
   useEffect(() => {
     localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos));
